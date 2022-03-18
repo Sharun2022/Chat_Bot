af495294b389fa56bb35f0387479d4171c2c6441
Sharun2022 <99365413+Sharun2022@users.noreply.github.com>
2022-03-19 11:38:41 +1300

    Created click and collect v1
    Created a basic function for click and collect version 1

---
+++ Components/click_collect v1.py
@@ -0,0 +1,15 @@
+#Bug - accepts blank input
+
+print ("Please enter the click and collect information")
+
+# Customer name
+name = input("Please enter your name ")
+
+
+# Phone number
+phone = input("Please enter your phone number ")
+
+print (name)
+print (phone)
+
+print ("You will recieve a text, when the food is ready for pickup")
