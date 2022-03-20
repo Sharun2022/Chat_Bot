ca04a636ce19e5c38450783c54a978630586fef3
Sharun2022 <99365413+Sharun2022@users.noreply.github.com>
2022-03-20 14:45:27 +1300

    Created Food Menu v2
    Created final version of food menu and created it as a function

---
+++ Components/food_menu v2.py
@@ -0,0 +1,13 @@
+food_names = ['Spring Rolls', 'Chicken Satay', 'Pork Belly', 'Teriyaki Chicken Sushi (8 Pack)', 'Chicken Curry', 
+'Mee Goreng','Deep Fried Prawns','Nasi Goreng', 'Tom Yum Soup', 'Pad Thai', 
+'Stir Fried Tofu with Rice', 'Whole Duck']
+
+food_prices = [5.00, 7.50, 10.50, 11.50, 13.50, 14.00, 14.50, 15.00, 15.50, 17.00, 18.50, 23.50]
+
+def menu():
+    number_food = 12
+
+    for count in range (number_food):
+        print("{} {} ${:.2f}"  .format(count+1, food_names[count], food_prices[count]))
+
+menu()
