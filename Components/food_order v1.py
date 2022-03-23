66e3965b813a7cbf0ca39e52b0f66a8f08ae4111
Sharun2022 <99365413+Sharun2022@users.noreply.github.com>
2022-03-24 10:47:58 +1300

    Created Food Order v1
    Presented a basic version of the menu to the user

---
+++ Components/food_order v1.py
@@ -0,0 +1,48 @@
+# List of Food names
+food_names = ['Spring Rolls', 'Chicken Satay', 'Pork Belly', 'Teriyaki Chicken Sushi (8 Pack)', 'Chicken Curry', 
+'Mee Goreng','Deep Fried Prawns','Nasi Goreng', 'Tom Yum Soup', 'Pad Thai', 
+'Stir Fried Tofu with Rice', 'Whole Duck'
+]
+# lists of Food Prices
+food_prices = [5.00, 7.50, 10.50, 11.50, 13.50, 14.00, 14.50, 15.00, 15.50, 17.00, 18.50, 23.50]
+
+#list to store ordered Food 
+order_list =[]
+#list to store Food prices
+order_cost = []
+
+#list to store order cost
+
+def menu():
+    number_food = 12
+
+    for count in range (number_food):
+        print("{} {} ${:.2f}"  .format(count+1, food_names[count], food_prices[count]))
+
+menu()
+
+# Ask for total number of pizzas for order
+num_food = 0 
+
+num_food = int(input("How many different food(s) do you want to order? "))
+
+print(num_food)
+
+# Choose food from menu
+print ("Please choose your food(s) by entering the number from the menu")
+for item in range(num_food):
+    while num_food > 0:
+        food_ordered = int(input())
+        order_list.append(food_names[food_ordered])
+        order_cost.append(food_prices[food_ordered])
+        num_food = num_food - 1
+
+print(order_list)
+print(order_cost)
+
+
+# Countdown until all pizzas are ordered 
+
+
+
+# Print order
--- chat_bot.py
+++ chat_bot.py
@@ -1,10 +1,20 @@
 # A bot for ordering food online
 
+import sys
 import random
 from random import randint
 
 # List of random names
 names = ["George", "Phoebe", "Sally",  "Michael", "Denise", "Ellen", "Moana", "Duncan", "Louis", "Danny"]
+
+# List of Food names
+food_names = ['Spring Rolls', 'Chicken Satay', 'Pork Belly', 'Teriyaki Chicken Sushi (8 Pack)', 'Chicken Curry', 
+'Mee Goreng','Deep Fried Prawns','Nasi Goreng', 'Tom Yum Soup', 'Pad Thai', 
+'Stir Fried Tofu with Rice', 'Whole Duck']
+
+# List of Food Prices
+food_prices = [5.00, 7.50, 10.50, 11.50, 13.50, 14.00, 14.50, 15.00, 15.50, 17.00, 18.50, 23.50]
+
 
 def welcome():
     '''
@@ -20,6 +30,40 @@ def welcome():
     print("*** I will be here to help you order your delicious food online ***")
 
 
+def order_type():
+    print("Is your order for click and collect or delivery?")
+    print ("For click and collect please enter 1")
+    print ("For delivery please enter 2")
+    while True:
+        try:
+            delivery = int(input("Please enter a number "))
+            if delivery >= 1 and delivery <= 2 :
+                if delivery == 1:
+                    print ("Click and Collect") 
+                    break 
+
+                elif delivery == 2:
+                    print ("Delivery")
+                    break
+            else: 
+                print ("The number must be 1 or 2")
+        except ValueError:
+            print ("That is not a valid number")
+            print ("Please enter 1 or 2")
+
+
+
+
+def menu():
+    number_food = 12
+
+    for count in range (number_food):
+        print("{} {} ${:.2f}"  .format(count+1, food_names[count], food_prices[count]))
+
+menu()
+
+
+
 def main():
     '''
     Purpose: To run all functions 
@@ -28,6 +72,7 @@ def main():
     Returns: None 
     '''
     welcome()
+    
 
 
 main()
