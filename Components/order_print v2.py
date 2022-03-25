a38ffd02e7ba2bb6e3349ba4aadd30948fb4412c
Sharun2022 <99365413+Sharun2022@users.noreply.github.com>
2022-03-26 12:34:43 +1300

    Created Order_Print v2
    Prints the details but requires more formatting

---
+++ Components/order_print v2.py
@@ -0,0 +1,18 @@
+#list to store ordered pizzas 
+order_list =['Spring Rolls', 'Mee Goreng', 'Pad Thai', 'Whole Duck']
+#list to store pizza prices
+order_cost = [5.00, 14.00, 17.00, 23.50]
+
+cust_details = {'name':'Mark','phone':'0213345656', 'house': '45', 'street':'Harry','suburb': 'Howick'}
+
+
+
+#print("\n",cust_details['name'],"\n",cust_details['phone'],"\n",cust_details['house'] "\n",cust_details['street'], "\n",cust_details['suburb'])
+
+#print("Customer Name: {} \nCustomer Phone: {} Customer House Number: \n{} Customer Street Name:\n{} Customer Suburb: \n{}" .format( cust_details['name'], cust_details['phone'], cust_details['house'], cust_details['street'], cust_details['suburb']))
+print("Customer Name: {} \nCustomer Phone: {} \nCustomer Address: {} {} {}" .format( cust_details['name'], cust_details['phone'], cust_details['house'], cust_details['street'], cust_details['suburb']))
+
+count = 0
+for item in order_list:
+    print("Ordered: {}  Cost ${:.2f}".format(item, order_cost[count]))
+    count = count + 1
